f318a62442c25f88135c558d9abdaae7e397b2f3
buddy <litoubuddy@gmail.com>
2019-05-04 02:01:52 +0800

    add: react-native-vector-icons

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 /**
  * Created by buddy on 2019-05-04.
  */
+
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+export default () => <Icon name='caretleft' size={20} colot='#ff8080' />;
